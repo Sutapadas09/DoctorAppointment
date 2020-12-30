e193776011df6734eb96dcf39f769f8a85f69605
Shreya Mukherjee <Shreya Mukherjee@LAPTOP-PQ94AO8A>
2020-12-30 13:25:47 +0530

    commit by shreya

---
+++ src/java/doctor/Doctor.java
@@ -0,0 +1,54 @@
+
+package doctor;
+
+
+public class Doctor {
+    private String name;
+    private String email;
+    private String phone;
+    private String specialization;
+    private String picture ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public void setPicture(String Picture) {
+        this.picture = picture;
+    }
+
+ public String getPicture() {
+        return picture;
+    }
+
+    
+
+    
+}
